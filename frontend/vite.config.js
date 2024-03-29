1c6ef14e134d8816a2556cae8030d8ce5966cbde
peter varga <pkvarg@yahoo.se>
2024-03-29 14:23:54 +0100

    localhost to 2001,2002

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,14 +6,17 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: 3000,
+    port: 2001,
+    //port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:6000',
+        target: 'http://localhost:2002',
+        //target: 'http://localhost:6000',
       },
 
       '/uploads': {
-        target: 'http://localhost:6000',
+        target: 'http://localhost:2002',
+        //target: 'http://localhost:6000',
       },
     },
     // hmr: {
