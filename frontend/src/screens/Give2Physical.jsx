b165c6ce7e80d47a714051e6acbfe5d6f558963a
peter varga <pkvarg@yahoo.se>
2025-02-19 18:03:02 +0100

    nixpacks toml

--- frontend/src/screens/Give2Physical.jsx
+++ frontend/src/screens/Give2Physical.jsx
@@ -3,10 +3,10 @@ import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Give2Physical = () => {
-  const file1 = '2023_priloha_c_1_ziadost_o_rocne_zuctovanie.docx'
-  const file2 = '2023_priloha_c_2_potvrdenie_o_zaplateni_dane.pdf'
-  const file3 = '2023_priloha_c_3_vyhlasenie_o_poukazani_dane.pdf'
-  const file4 = '2023_priloha_c_3a_poucenie_k_vyhlaseniu_o_poukazani_dane.pdf'
+  const file1 = '2024_priloha_c_1_ziadost_o_rocne_zuctovanie.pdf'
+  const file2 = '2024_priloha_c_2_potvrdenie_o_zaplateni_dane.pdf'
+  const file3 = '2024_priloha_c_3_vyhlasenie_o_poukazani_dane.pdf'
+  const file4 = '2024_priloha_c_4_vyhlasenie_o_poukazani_dane.pdf'
   const downloadFileHandler = (fileName) => {
     console.log(fileName)
     fetch(`${fileName}`).then((response) => {
@@ -23,16 +23,16 @@ const Give2Physical = () => {
   }
 
   return (
-    <div className='margined'>
-      <Link className='btn btn-back my-3' to='/'>
+    <div className="margined">
+      <Link className="btn btn-back my-3" to="/">
         Naspäť
       </Link>
-      <div className='give2-physical'>
-        <h1 className='my-5'>FYZICKÉ OSOBY</h1>
+      <div className="give2-physical">
+        <h1 className="my-5">FYZICKÉ OSOBY</h1>
         <strong>Postup pre fyzické osoby – zamestnancov</strong>
         <p>
           (Postup pre fyzické osoby, ktoré si samy podávajú daňové priznanie
-          <a href='/give2-physical-business' className='no-underline blue '>
+          <a href="/give2-physical-business" className="no-underline blue ">
             {' '}
             nájdete tu
           </a>
@@ -41,100 +41,78 @@ const Give2Physical = () => {
 
         <ul>
           <li>
-            Do 15.02.2023 požiadajte svojho zamestnávateľa o vykonanie ročného
-            zúčtovania zaplatených preddavkov na daň (príloha č. 1 Žiadosť o
-            vykonanie ročného zúčtovania preddavkov).
+            Do 17.02.2025 požiadajte svojho zamestnávateľa o vykonanie ročného zúčtovania
+            zaplatených preddavkov na daň (príloha č. 1 Žiadosť o vykonanie ročného zúčtovania
+            preddavkov).
           </li>
           <li>
-            Po vykonaní zúčtovania požiadajte zamestnávateľa, aby Vám vystavil
-            tlačivo Potvrdenie o zaplatení dane za minulý rok (príloha č. 2
-            Potvrdenie o zaplatení dane).
+            Po vykonaní zúčtovania požiadajte zamestnávateľa, aby Vám vystavil tlačivo Potvrdenie o
+            zaplatení dane za minulý rok (príloha č. 2 Potvrdenie o zaplatení dane).
           </li>
           <li>
-            Vyplňte Vyhlásenie o poukázaní 2% z dane pre OZ Prúd (príloha č. 3
-            Vyhlásenie o poukázaní dane) podľa poučenia (príloha č. 3a Poučenie
-            k Vyhláseniu o poukázaní dane).
+            Vyplňte Vyhlásenie o poukázaní 2% z dane pre OZ Prúd (príloha č. 3 Vyhlásenie o
+            poukázaní dane) podľa poučenia (príloha č. 3a Poučenie k Vyhláseniu o poukázaní dane).
           </li>
           <li>
-            Z Potvrdenia o zaplatení dane zistíte potrebné údaje na vyplnenie
-            Vyhlásenia o poukázaní dane, t. j. dátum zaplatenia dane a sumu
-            zaplatenej dane na výpočet príslušných percent:
+            Z Potvrdenia o zaplatení dane zistíte potrebné údaje na vyplnenie Vyhlásenia o poukázaní
+            dane, t. j. dátum zaplatenia dane a sumu zaplatenej dane na výpočet príslušných percent:
             <p>
-              1. 2% zo svojej zaplatenej dane - ide o maximálnu sumu, ktorú
-              môžete poukázať v prípade, že ste v minulom roku neboli
-              dobrovoľníkom, alebo ste dobrovoľnícky odpracovali menej ako 40
-              hodín. Minimálna výška tejto sumy je však 3 €.
+              1. 2% zo svojej zaplatenej dane - ide o maximálnu sumu, ktorú môžete poukázať v
+              prípade, že ste v minulom roku neboli dobrovoľníkom, alebo ste dobrovoľnícky
+              odpracovali menej ako 40 hodín. Minimálna výška tejto sumy je však 3 €.
             </p>
             <p>
-              2. 3% zo svojej zaplatenej dane – týka sa len osôb, ktoré v
-              minulom roku dobrovoľnícky odpracovali minimálne 40 hodín.
-              Potvrdenie o tom od organizácie, resp. organizácií, v ktorých ste
-              pôsobili, je povinnou prílohou k vyhláseniu a potvrdeniu o
-              zaplatení dane.
+              2. 3% zo svojej zaplatenej dane – týka sa len osôb, ktoré v minulom roku dobrovoľnícky
+              odpracovali minimálne 40 hodín. Potvrdenie o tom od organizácie, resp. organizácií, v
+              ktorých ste pôsobili, je povinnou prílohou k vyhláseniu a potvrdeniu o zaplatení dane.
             </p>
           </li>
           <li>
-            Vyplnené Vyhlásenie o poukázaní 2% a tlačivo Potvrdenie o zaplatení
-            dane (dobrovoľníci s počtom aspoň 40 odpracovaných hodín za minulý
-            rok aj s potvrdením od príslušnej organizácie, resp. organizácií)
-            doručte do 30.04.2023 na Daňový úrad podľa miesta svojho trvalého
-            bydliska, nie podľa sídla zamestnávateľa.
+            Vyplnené Vyhlásenie o poukázaní 2% a tlačivo Potvrdenie o zaplatení dane (dobrovoľníci s
+            počtom aspoň 40 odpracovaných hodín za minulý rok aj s potvrdením od príslušnej
+            organizácie, resp. organizácií) doručte do 30.04.2025 na Daňový úrad podľa miesta svojho
+            trvalého bydliska, nie podľa sídla zamestnávateľa.
           </li>
           <li>
-            Po splnení podmienok majú daňové úrady tri mesiace na prevedenie 2%
-            alebo 3% v prospech občianskeho združenia Prúd.
+            Po splnení podmienok majú daňové úrady tri mesiace na prevedenie 2% alebo 3% v prospech
+            občianskeho združenia Prúd.
           </li>
         </ul>
-        <div className='my-5'>
+        <div className="my-5">
           <strong>Potrebné tlačivá nájdete pod článkom</strong>
           <p>Príloha č. 01 - Žiadosť o ročné zúčtovanie</p>
           <p>Príloha č. 02 - Potvrdenie o zaplatení dane</p>
           <p>Príloha č. 03 - Vyhlásenie o poukázaní dane</p>
           <p>Príloha č. 03a - Poučenie k Vyhláseniu o poukázaní dane</p>
         </div>
-        <div className='my-5'>
+        <div className="my-5">
           <strong>Úpravy pre dobrovoľníkov</strong>
           <p>
-            V prípade, že fyzická osoba (zamestnanec alebo živnostník) pre
-            akúkoľvek neziskovú organizáciu alebo jednotlivca (prijímateľa
-            dobrovoľníckej činnosti) počas minulého roka odpracovala ako
-            dobrovoľník aspoň 40 hodín a táto organizácia/jednotlivec o tom vydá
-            Potvrdenie o výkone dobrovoľníckej činnosti, môže fyzická osoba
-            venovať až 3% zo svojej dane! V tom prípade vo Vyhlásení vypočítajte
-            3% zo svojej dane a k Vyhláseniu priložte okrem Potvrdenia o
-            zaplatení dane aj Potvrdenie o výkone dobrovoľníckej činnosti.
-            Dobrovoľník nemusel odpracovať 40 hodín výlučne v prospech jedinej
-            organizácie/ jednotlivca. Počet minimálne 40 hodín tak môže
-            „vyskladať“ aj z viacerých Potvrdení od viacerých
-            organizácií/jednotlivcov.
+            V prípade, že fyzická osoba (zamestnanec alebo živnostník) pre akúkoľvek neziskovú
+            organizáciu alebo jednotlivca (prijímateľa dobrovoľníckej činnosti) počas minulého roka
+            odpracovala ako dobrovoľník aspoň 40 hodín a táto organizácia/jednotlivec o tom vydá
+            Potvrdenie o výkone dobrovoľníckej činnosti, môže fyzická osoba venovať až 3% zo svojej
+            dane! V tom prípade vo Vyhlásení vypočítajte 3% zo svojej dane a k Vyhláseniu priložte
+            okrem Potvrdenia o zaplatení dane aj Potvrdenie o výkone dobrovoľníckej činnosti.
+            Dobrovoľník nemusel odpracovať 40 hodín výlučne v prospech jedinej organizácie/
+            jednotlivca. Počet minimálne 40 hodín tak môže „vyskladať“ aj z viacerých Potvrdení od
+            viacerých organizácií/jednotlivcov.
           </p>
         </div>
 
-        <div className='files-btns my-5'>
+        <div className="files-btns my-5">
           <strong>Súbory na stiahnutie:</strong>
 
-          <Button
-            className='btn-file'
-            onClick={() => downloadFileHandler(file1)}
-          >
+          <Button className="btn-file" onClick={() => downloadFileHandler(file1)}>
             Žiadosť o ročné zúčtovanie
           </Button>
-          <Button
-            className='btn-file'
-            onClick={() => downloadFileHandler(file2)}
-          >
+          <Button className="btn-file" onClick={() => downloadFileHandler(file2)}>
             Potvrdenie o zaplatení dane
           </Button>
-          <Button
-            className='btn-file'
-            onClick={() => downloadFileHandler(file3)}
-          >
+          <Button className="btn-file" onClick={() => downloadFileHandler(file3)}>
             Vyhlásenie o poukázaní dane
           </Button>
-          <Button
-            className='btn-file'
-            onClick={() => downloadFileHandler(file4)}
-          >
+          <Button className="btn-file" onClick={() => downloadFileHandler(file4)}>
             Poučenie k vyhláseniu o poukázaní dane
           </Button>
         </div>
